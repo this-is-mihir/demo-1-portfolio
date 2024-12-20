f8c151aa9562ed7819870007bfdd259719714596
this-is-mihir <patelmihir0367@gmail.com>
2024-12-20 09:51:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// ================================== typing animation ============================
+
+var typed =new Typed(".typing", {
+    strings:["","Web Developer","web Designer","Blog Writer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
